ddfe03114c29d81e9ec24ce7de6b4528a04a4d67
stack72 <public@paulstack.co.uk>
2013-06-16 22:22:25 +0200

    added validation of the parameters

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,5 +1,9 @@
 define teamcity::plugin($plugin = $title, $data_directory, $restart) {
 
+  validate_bool($restart)
+  validate_string($plugin)
+  validate_re($plugin,['^(.)+$'], 'plugin name must not be empty')
+
   file { "Ensure-${plugin}-present":
     ensure  => file,
     path    => "${data_directory}/plugins/${plugin}",

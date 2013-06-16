cc41be744ac21dfacef3d77b20bc9249e6cc6ce4
stack72 <public@paulstack.co.uk>
2013-06-16 22:18:02 +0200

    changing the location of the module namespace

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -3,7 +3,7 @@ define teamcity::plugin($plugin = $title, $data_directory, $restart) {
   file { "Ensure-${plugin}-present":
     ensure  => file,
     path    => "${data_directory}/plugins/${plugin}",
-    source  => "puppet:///modules/teamcity_plugins/${plugin}",
+    source  => "puppet:///modules/teamcity/${plugin}",
     #notify  => Exec['restart_teamcity'],
   }
 

78580dbc9e11d5460ade870107c1e346dbf2f4d1
stack72 <public@paulstack.co.uk>
2013-06-16 22:59:30 +0200

    changed the formatting of the sample init file to follow the correct lint specification

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class teamcity {
 
-$teamcity_data_directory = "c:/temp"
+  $teamcity_data_directory = 'c:/temp'
 
-  manage_plugin {"jonnyzzz.node.zip":
+  teamcity::manage_plugin {'jonnyzzz.node.zip':
     data_directory => $teamcity_data_directory,
   }
 
-  manage_plugin {"another.plugin.zip":
+  teamcity::manage_plugin {'another.plugin.zip':
     data_directory => $teamcity_data_directory,
     restart        => true
   }

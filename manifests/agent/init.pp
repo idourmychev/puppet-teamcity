4779890341126ccb0b2ab69e01c26b46a5267def
stack72 <public@paulstack.co.uk>
2014-05-07 13:58:51 +0100

    Creation of the new module structure and the private classes that will make that structure up

---
+++ manifests/agent/init.pp
@@ -0,0 +1,3 @@
+class teamcity::agent {
+
+}
---
+++ manifests/agent/service.pp
@@ -0,0 +1,5 @@
+class teamcity::agent::service {
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+}
